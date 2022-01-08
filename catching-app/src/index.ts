5dc676083404e0fc0b34156ab9f2fce5d49d4243
dparker66 <dparker@gatech.edu>
2022-01-08 12:22:19 -0800

    remake frontend with webpack + ts

---
+++ catching-app/src/index.ts
@@ -0,0 +1,8 @@
+import "aframe"
+import "aframe-look-at-component"
+import "aframe-state-component"
+import "aframe-randomizer-components"
+import "aframe-template-component"
+import "@ar-js-org/ar.js"
+
+console.log("Hello World!");
